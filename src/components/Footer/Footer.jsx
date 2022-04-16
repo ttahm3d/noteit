2051078bc69cca0eca668a3501b6224c54ba0493
ttahm3d <ttahm3d@gmail.com>
2022-04-16 20:41:34 +0530

    feat: created footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import NoteIt from "../../assets/NoteIt.svg";
+
+export default function () {
+  return (
+    <FooterContainer>
+      <Logo>
+        <img src={NoteIt} alt="NoteIt Logo" />
+      </Logo>
+    </FooterContainer>
+  );
+}
+
+const FooterContainer = styled.footer`
+  padding: 2rem 1rem;
+  margin-top: auto;
+  box-shadow: 0 0 4px ${(props) => props.theme.colors.gray6};
+`;
+
+const Logo = styled.div``;
