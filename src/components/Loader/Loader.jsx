9aeba2121c88b613507a474b354ebb8c5e110ff8
ttahm3d <ttahm3d@gmail.com>
2022-04-16 20:41:45 +0530

    feat: created loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,34 @@
+import styled, { keyframes } from "styled-components";
+
+export default function () {
+  return (
+    <LoaderContainer>
+      <LoaderRing />
+    </LoaderContainer>
+  );
+}
+
+const LoaderContainer = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 90vh;
+`;
+
+const spinAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoaderRing = styled.div`
+  border: 0.5rem solid var(--gray3);
+  border-top: 0.5rem solid var(--blue10);
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  animation: ${spinAnimation} 2s linear infinite;
+`;
