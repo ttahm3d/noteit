6e6f1d0d83ddbf70e1f828687bdc2bbc1181839d
ttahm3d <ttahm3d@gmail.com>
2022-04-21 23:07:16 +0530

    feat: exporting buttons

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Loader from "./Loader/Loader";
 import ScrollToTop from "./ScrollToTop/ScrollToTop";
 import Sidebar from "./Sidebar/Sidebar";
+import { IconButton, Button } from "./Button/Button";
 
-export { Header, Footer, Loader, ScrollToTop, Sidebar };
+export { Header, Footer, Loader, ScrollToTop, Sidebar, IconButton, Button };
