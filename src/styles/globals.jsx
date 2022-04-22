5e9276be2cf1bd408837ed07c7c8a9930362f1a5
ttahm3d <ttahm3d@gmail.com>
2022-04-22 22:37:05 +0530

    style - removed height from main container

--- src/styles/globals.jsx
+++ src/styles/globals.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const MainContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  min-height: 100vh;
 
   /* @media screen and (min-width: 64em) {
     grid-template-columns: 1fr 5fr;
