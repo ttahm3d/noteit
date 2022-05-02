c4f9d80ffa80f42f55fde3c0f86f9c5866000ba3
ttahm3d <ttahm3d@gmail.com>
2022-05-02 14:01:42 +0530

    chore : removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Footer, Header, ScrollToTop, Sidebar } from "./components";
 import { useLocalStorage } from "./hooks";
 import Router from "./router/Router";
 import { MainContainer, Page } from "./styles/globals";
-import { GlobalStyle, lightTheme, darkTheme, light } from "./styles/theme";
+import { GlobalStyle, lightTheme, darkTheme } from "./styles/theme";
 
 function App() {
   const [theme, setTheme] = useLocalStorage("noteit-theme");
