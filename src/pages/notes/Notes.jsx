3320a9bad69be806c462af23778b105c2e80e3bb
ttahm3d <ttahm3d@gmail.com>
2022-05-02 14:00:19 +0530

    chore: sample work for modal

--- src/pages/notes/Notes.jsx
+++ src/pages/notes/Notes.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { Button } from "../../components";
+import Modal from "../../components/Modal/Modal";
 import { Container } from "../../styles/globals";
 
 export default function Notes() {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => setShowModal((s) => !s);
+  const closeModal = () => setShowModal(false);
+
   return (
     <Container>
       <h3>Notes page</h3>
+      <Button variant="primary__block" onClick={toggleModal}>
+        Text
+      </Button>
+      <Modal
+        showModal={showModal}
+        header="sample header"
+        closeModal={closeModal}>
+        This is sample modal
+      </Modal>
     </Container>
   );
 }
