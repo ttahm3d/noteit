ae3431df16115383d406b0253eba66dbad07fab9
ttahm3d <ttahm3d@gmail.com>
2022-05-06 23:17:11 +0530

    feat: hooked all input to state

--- src/components/NoteForm/NoteForm.jsx
+++ src/components/NoteForm/NoteForm.jsx
@@ -1,36 +1,58 @@
 import styled from "styled-components";
 import ReactQuill from "react-quill";
-import Input from "../Input/Input";
 import "react-quill/dist/quill.snow.css";
 import { useState } from "react";
 
 export default function NoteForm() {
-  const [title, setTitle] = useState("");
+  const [note, setNote] = useState({
+    title: "",
+    color: "gray",
+    body: "",
+  });
+  const colorsList = ["yellow", "crimson", "violet", "orange", "green", "gray"];
+
+  console.log(note);
 
   const modules = {
     toolbar: [
-      ["bold", "italic", "underline", "strike", "blockquote"],
+      ["bold", "italic", "underline", "blockquote"],
       [
         { list: "ordered" },
         { list: "bullet" },
         { indent: "-1" },
         { indent: "+1" },
       ],
-      ["link", "image"],
-      ["clean"],
     ],
   };
+
   return (
-    <NotesContainer>
+    <NotesContainer color={note.color}>
       <TitleInput
-        value={title}
+        value={note.title}
         required={true}
         name="title"
         type="text"
+        color={note.color}
         placeholder="Enter Title"
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={(e) => setNote({ ...note, title: e.target.value })}
       />
-      <ReactQuill modules={modules} placeholder="Enter Note..." />
+      <ReactQuill
+        modules={modules}
+        placeholder="Enter Note..."
+        value={note.body}
+        preserveWhitespace={true}
+        onChange={(e) => setNote({ ...note, body: e })}
+      />
+      <ColorPickerContainer>
+        {colorsList.map((notecolor) => (
+          <ColorCircle
+            notecolor={notecolor}
+            color={note.color}
+            key={notecolor}
+            onClick={() => setNote({ ...note, color: notecolor })}
+          />
+        ))}
+      </ColorPickerContainer>
     </NotesContainer>
   );
 }
@@ -41,12 +63,12 @@ const NotesContainer = styled.div`
     display: flex;
     flex-direction: column;
     font-family: inherit;
-    background-color: ${(props) => props.theme.colors.orange4};
-    color: ${(props) => props.theme.colors.orange10};
+    background-color: ${({ theme, color }) => `${theme.colors[color + "5"]}`};
+    color: ${({ theme, color }) => `${theme.colors[color + "12"]}`};
   }
 
   .ql-editor.ql-blank::before {
-    color: ${(props) => props.theme.colors.orange11};
+    color: ${({ theme, color }) => `${theme.colors[color + "11"]}`};
     text-decoration: none;
   }
 
@@ -55,6 +77,10 @@ const NotesContainer = styled.div`
     font-size: 1rem;
     font-family: inherit;
     border: none;
+
+    :focus {
+      background-color: ${({ theme, color }) => `${theme.colors[color + "4"]}`};
+    }
   }
 
   /* toolbar icons container */
@@ -62,7 +88,7 @@ const NotesContainer = styled.div`
     display: flex;
     padding: 10px;
     border: none;
-    background-color: ${(props) => props.theme.colors.orange3};
+    background-color: ${({ theme, color }) => `${theme.colors[color + "5"]}`};
   }
 
   /* toolbar icons wrapper*/
@@ -73,13 +99,13 @@ const NotesContainer = styled.div`
   }
 
   .ql-toolbar button {
-    fill: ${(props) => props.theme.colors.orange11};
+    fill: ${({ theme, color }) => `${theme.colors[color + "11"]}`};
   }
 
   /* Active toolbar icon */
   .ql-toolbar button.ql-active {
-    background-color: ${(props) => props.theme.colors.orange5};
-    border: 1px solid ${(props) => props.theme.colors.orange7};
+    background-color: ${({ theme, color }) => `${theme.colors[color + "5"]}`};
+    border: 1px solid ${({ theme, color }) => `${theme.colors[color + "7"]}`};
     border-radius: 0.25rem;
   }
 
@@ -89,44 +115,61 @@ const NotesContainer = styled.div`
 
   .ql-toolbar button .ql-stroke,
   .ql-picker-label .ql-stroke {
-    stroke: ${(props) => props.theme.colors.orange11};
+    stroke: ${({ theme, color }) => `${theme.colors[color + "11"]}`};
   }
 
   .ql-snow.ql-toolbar button:hover .ql-stroke,
   .ql-snow.ql-toolbar .ql-picker-label:hover .ql-stroke,
   .ql-snow .ql-stroke,
   .ql-snow.ql-toolbar button.ql-active .ql-stroke {
-    stroke: ${(props) => props.theme.colors.orange11};
+    stroke: ${({ theme, color }) => `${theme.colors[color + "11"]}`};
   }
 
   .ql-snow.ql-toolbar button:hover .ql-fill,
   .ql-snow.ql-toolbar .ql-picker-label:hover .ql-fill,
   .ql-snow.ql-toolbar button.ql-active .ql-fill {
-    fill: ${(props) => props.theme.colors.orange11};
+    fill: ${({ theme, color }) => `${theme.colors[color + "11"]}`};
   }
 `;
 
 const TitleInput = styled.input`
-  padding: 0.25rem;
-  border-radius: 0.25rem;
+  padding: 1rem;
   font-size: 1rem;
   width: 100%;
-  background-color: ${(props) => props.theme.colors.orange3};
-  border: 1px solid ${(props) => props.theme.colors.orange7};
-  color: ${(props) => props.theme.colors.orange11};
+  background-color: ${({ theme, color }) => `${theme.colors[color + "3"]}`};
+  border: 0;
+  color: ${({ theme, color }) => `${theme.colors[color + "11"]}`};
 
   ::placeholder {
-    color: ${(props) => props.theme.colors.orange11};
+    color: ${({ theme, color }) => `${theme.colors[color + "11"]}`};
+    font-style: italic;
   }
 
   :focus {
-    background-color: ${(props) => props.theme.colors.orange5};
-    outline: 1px solid ${(props) => props.theme.colors.orange8};
+    outline: 1px solid transparent;
     outline-offset: -2px;
   }
 
   :disabled {
-    background-color: ${(props) => props.theme.colors.orange3};
+    background-color: ${({ theme, color }) => `${theme.colors[color + "3"]}`};
     cursor: not-allowed;
   }
+`;
+
+const ColorPickerContainer = styled.div`
+  display: flex;
+  padding: 1rem;
+  gap: 1rem;
+`;
+
+const ColorCircle = styled.div`
+  padding: 0.75rem;
+  cursor: pointer;
+  background-color: ${({ theme, notecolor }) =>
+    `${theme.colors[notecolor + "9"]}`};
+  border-radius: 50%;
+
+  outline: ${({ theme, notecolor, color }) =>
+    notecolor === color && `3px solid ${theme.colors[notecolor + "7"]}`};
+  outline-offset: 0.25rem;
 `;
