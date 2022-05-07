72a1717cf1fe55eb6e129ffcbf6ba12b138a8739
ttahm3d <ttahm3d@gmail.com>
2022-05-07 18:04:04 +0530

    feat: created and using context for notes

--- src/context/notes/Context.jsx
+++ src/context/notes/Context.jsx
@@ -1,7 +1,31 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import { supabase } from "../../SupabaseClient";
+import { useAuth } from "../auth";
 
 const NotesContext = createContext();
 
 const NotesProvider = ({ children }) => {
-  return <NotesContext.Provider>{children}</NotesContext.Provider>;
+  const { user } = useAuth();
+
+  const addNote = async (note) => {
+    const { data, error } = await supabase.from("notes").insert([
+      {
+        title: note.title,
+        body: note.body,
+        color: note.color,
+        userId: user?.id,
+      },
+    ]);
+    console.log(data, error);
+  };
+
+  return (
+    <NotesContext.Provider value={{ addNote }}>
+      {children}
+    </NotesContext.Provider>
+  );
 };
+
+const useNotes = () => useContext(NotesContext);
+
+export { NotesProvider, useNotes };
