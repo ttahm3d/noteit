bf86580aea6140ad5e09865102ed74fd87984135
ttahm3d <ttahm3d@gmail.com>
2022-05-16 19:26:18 +0530

    feat: implemented trash page

--- src/pages/trash/Trash.jsx
+++ src/pages/trash/Trash.jsx
@@ -1,9 +1,70 @@
+import { useState } from "react";
 import { Container } from "../../styles/globals";
+import {
+  Button,
+  Empty,
+  Loader,
+  Modal,
+  NoteCard,
+  NoteForm,
+} from "../../components";
+import { AiOutlineDelete } from "react-icons/ai";
+import { IoArchiveOutline } from "react-icons/io5";
+import { useNotes } from "../../context/notes";
+import { NotesContainer, Content } from "../styles/NotePage.styles";
 
 export default function Trash() {
+  const [isEdit, setIsEdit] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    color: "blue",
+    body: "",
+  });
+  const { notes, loading } = useNotes();
+
+  const trashedNotes = notes.filter((note) => note.isTrashed);
+
+  const toggleModal = () => setShowModal((s) => !s);
+  const closeModal = () => setShowModal(false);
+  const noteActions = [
+    {
+      id: "trash",
+      icon: <AiOutlineDelete />,
+      title: "Move to Trash",
+      // actionHandler: moveToTrash,
+    },
+    {
+      id: "archive",
+      icon: <IoArchiveOutline />,
+      title: "Archive Note",
+      // actionHandler: moveToArchive,
+    },
+  ];
+
+  if (loading) return <Loader />;
+
   return (
     <Container>
-      <h3>Trash page</h3>
+      <Content>
+        <h3>Trash page</h3>
+        {trashedNotes.length > 0 ? (
+          <NotesContainer>
+            {trashedNotes.map((note) => (
+              <NoteCard
+                note={note}
+                setNote={setNote}
+                setIsEdit={setIsEdit}
+                toggleModal={toggleModal}
+                noteActions={noteActions}
+                key={note.id}
+              />
+            ))}
+          </NotesContainer>
+        ) : (
+          <Empty />
+        )}
+      </Content>
     </Container>
   );
 }
